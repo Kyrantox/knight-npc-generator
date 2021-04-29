1e097528a98a39391aa22868f848a6623f49e11d
David Reignier <david.reignier@scantech.com>
2021-04-29 14:24:29 +0200

    Create a tool to generate a bestiary

--- src/app/bestiary/bestiary.component.ts
+++ src/app/bestiary/bestiary.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Npc } from '../model/Npc';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-bestiary',
@@ -7,32 +9,98 @@ import { Npc } from '../model/Npc';
   styleUrls: ['./bestiary.component.scss']
 })
 export class BestiaryComponent implements OnInit {
-
-  title: string = 'Meilleur bestiaire du monde';
+  title: string = '';
   list: Npc[] = [];
   data: { npc: Npc; description: string }[] = [];
   bestiary: { npc: Npc; description: string }[] = [];
+  changed: Subject<void> = new Subject<void>();
 
   constructor() {
-    const json = localStorage.getItem('list');
-
+    let json = localStorage.getItem('list');
     if (json) {
       const data = JSON.parse(json);
       this.list = data.map((e: any) => new Npc(e));
-
-      this.generate();
     }
+
+    json = localStorage.getItem('bestiary.cache');
+    if (json) {
+      const data = JSON.parse(json);
+
+      this.title = data.title ?? '';
+      this.data = data.data ?? [];
+    }
+
+    this.list = this.list.filter(npc => !this.data.some(line => line.npc.name === npc.name));
+
+    this.changed.pipe(debounceTime(1000)).subscribe(() => {
+      console.log('save');
+      localStorage.setItem('bestiary.cache', JSON.stringify({ title: this.title, data: this.data }));
+    });
+  }
+
+  add(npc: Npc) {
+    this.list = this.list.filter(n => n !== npc);
+
+    this.data.push({ npc, description: '' });
+
+    this.change();
+  }
+
+  remove(line: { npc: Npc; description: string }) {
+    this.data = this.data.filter(l => l !== line);
+
+    this.list.push(line.npc);
+    this.list.sort((a, b) => a.name.localeCompare(b.name));
+
+    this.change();
+  }
+
+  change() {
+    this.changed.next();
   }
 
   generate() {
-    this.bestiary = this.list.map(npc => ({ npc, description: 'Coucou je suis une super description Coucou je suis une super description Coucou je suis une super description Coucou je suis une super description Coucou je suis une super description Coucou je suis une super description Coucou je suis une super description Coucou je suis une super description Coucou je suis une super description Coucou je suis une super description Coucou je suis une super description Coucou je suis une super description Coucou je suis une super description '}));
+    this.bestiary = this.data.map(e => ({ npc: new Npc(e.npc), description: e.description }));
+
+    if (!this.bestiary.length) {
+      return;
+    }
 
     setTimeout(() => {
       const blocks = Array.from(document.querySelectorAll('#bestiary .npc-block-wrap'));
 
       for (const block of blocks) {
-        (<any> block).style.height = (block.children[0].clientHeight * 0.7) + 'px';
+        const style = (<any> block).style;
+        style.height = (block.children[0].clientHeight * 0.7) + 'px';
+        style.width = (block.children[0].clientWidth * 0.7) + 'px';
       }
+
+      const pages = Array.from(document.querySelectorAll('.page-content'));
+
+      for (const page of pages) {
+        if (page.children[0].clientHeight + 20 > page.clientHeight) {
+          page.querySelector('.page-npc-content')!.classList.add('two-columns');
+        }
+      }
+
+      for (const page of pages) {
+        if (page.children[0].clientHeight + 20 > page.clientHeight) {
+          console.log('fuck ?');
+          const npc = page.querySelector('.npc-block-wrap');
+
+          if (npc) {
+            const diff = page.children[0].clientHeight + 20 - page.clientHeight;
+
+            const ratio = diff / npc.clientHeight;
+
+            const style = (<any> npc).style;
+            style['margin-left'] = -(npc.clientWidth * ratio) + 'px';
+            style.transform = `scale(${1 - ratio})`;
+          }
+        }
+      }
+
+      setTimeout(() => window.scrollTo(0, document.getElementById('bestiary')!.getBoundingClientRect().top + window.scrollY - 50), 1);
     }, 1);
   }
 
